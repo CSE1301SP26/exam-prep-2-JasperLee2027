294a0bfcb2b886821ef5705cdee4dd4dec9abc01
JasperLee2027 <jasper.l@wustl.edu>
2026-03-18 21:10:58 -0500

    Update Q06.java

--- src/exam/codewriting/Q06.java
+++ src/exam/codewriting/Q06.java
@@ -25,14 +25,25 @@ public class Q06 {
 	//You should use a helper method as part of your recursive implementation.
 	
 	public static double sqrtR( double x) {
-		return 0; //fix me
+		return sqrtRHelper (x, x, x/2);
 	}
-
+	
+	public static double sqrtRHelper(double x, double xOld, double xNew) {
+		
+		if (Math.abs(xNew-xOld))<0.001) {
+			return (xNew);
+		}
+		
+		double newOld= xNew;
+		double newNew= ( xNew + (x / xNew )) / 2;
+		
+		return sqrtRHelper (x, newOld, newNew);
+	}
 
 	public static void main ( String[] args ) {
 		System.out.println(sqrt(5)); //given method
 		
-		System.out.println(sqrt(5)); //your recursive method
+		System.out.println(sqrtR(5)); //your recursive method
 		
 	}
 
